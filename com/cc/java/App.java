a7096a0e94eda2b35bcf56e4b4175e41d29b3027
yoda <dlesnik23@gmail.com>
2022-04-14 09:28:20 +0200

    doYourJob() / overriding

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,13 +4,31 @@ public class App {
     
     public static void main(String[] args) {
      
+        Queen queen = new Queen();
+        Worker worker = new Worker();
+        Drone drone = new Drone();
+
+        output(queen.doYourJob());
+        output(worker.doYourJob());
+        output(drone.doYourJob());
+
+        output("-----------------");
+
+        output(queen.fly());
+        output(worker.fly());
+        output(drone.fly());
+
     }
 
     public static void output(String outputStr) {
         System.out.println(outputStr);
-    }
+        
+
+        
 
 
+
+        }
 
 }
 
---
+++ com/cc/java/HoneyBee.java
@@ -0,0 +1,17 @@
+package com.cc.java;
+
+public class HoneyBee {
+    
+
+
+    public String doYourJob() {
+        return "I´m a HoneyBee!";
+    }
+
+
+
+    public String fly() {
+        return "I can fly!";
+    }
+
+}
